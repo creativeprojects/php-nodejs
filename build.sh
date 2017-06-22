205947d5671735718b0287b758d2104c30fcf74c
Fred <Fred@CreativeProjects.Tech>
2017-06-22 15:10:36 +0100

    Build script

--- build.sh
+++ build.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
 image_name=creativeprojects/php-nodejs
+image_versions="5.6 7.0 7.1"
 
-php_version=5.6
+for image_version in ${image_versions}; do
+    echo Downloading php:${image_version}
+    docker pull php:${image_version}
 
-docker pull php:${php_version}
-docker rmi ${image_name}:${php_version}
-docker rmi ${image_name}:latest
-docker build -t ${image_name}:${php_version} -t ${image_name}:latest -f Dockerfile-php${php_version} ./
-docker push ${image_name}:${php_version}
-docker push ${image_name}:latest
+    # docker rmi ${image_name}:${image_version}
+    # docker rmi ${image_name}:latest
+
+    echo Building image ${image_name}:${image_version}
+    docker build -t ${image_name}:${image_version} \
+                 -t ${image_name}:latest \
+                 -f Dockerfile-php${image_version} \
+                 ./
+
+    echo Pushing image ${image_name}:${image_version}
+    docker push ${image_name}:${image_version}
+
+    echo Pushing image ${image_name}:latest
+    docker push ${image_name}:latest
+done
